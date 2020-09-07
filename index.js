b57f98f828fb9e484a36bc4110d85bd566e741a8
Anguiar <shacharroy14@gmail.com>
2020-09-07 13:09:18 +0300

    build: removed unnecessery files/directories

--- index.js
+++
@@ -1,3 +0,0 @@
-const server = require('./src/server');
-
-server.listen(1337, function() { console.log('Listening on port 1337') });
--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const serverInstance = express();
-
-serverInstance.get('/helloworld', async function(req, res) {
-    return res.status(200).json({ hello: 'world' });
-});
-
-module.exports = serverInstance;
